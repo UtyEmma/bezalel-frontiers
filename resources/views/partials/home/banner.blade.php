4d7ac016fb7dd84c328f67ef0580664164c3aba4
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2024-06-28 11:21:31 +0100

    Set up to push to production

--- resources/views/partials/home/banner.blade.php
+++ resources/views/partials/home/banner.blade.php
@@ -4,7 +4,7 @@
             @forelse ($sliders as $slider)
                 <div class="swiper-slide">
                     <div class="slide-inner slide-bg-image bg-[url({{$slider->image}})] before:absolute before:left-0
-                    before:top-0 before:w-full before:h-full before:bg-[#040128] before:opacity-[0.4] "
+                    before:top-0 before:w-full before:h-full before:bg-[#040128] before:opacity-[0.7] "
                         data-background="{{$slider->image}}">
                         <!-- <div class="gradient-overlay"></div> -->
                         <div class="wraper">
@@ -33,40 +33,32 @@
                 </div>
             @empty
                 <div class="swiper-slide">
-                    <div class="slide-inner slide-bg-image before:absolute before:left-0
-                    before:top-0 before:w-full before:h-full before:bg-[#040128] before:opacity-[0.4] "
-                        data-background="assets/images/slider/slide-4.jpg">
+                    <div class="slide-inner slide-bg-image before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-[#040128] before:opacity-[0.7]" data-background="{{asset('assets/imgs/bezalel-frontiers-business-meeting.jpg')}}" >
                         <!-- <div class="gradient-overlay"></div> -->
                         <div class="wraper">
                             <div class="relative h-[900px] lg:h-[680px]  md:h-[600px] sm:h-[500px]">
-                                <div
-                                    class="max-w-[720px] lg:w-[600px] md:w-full  absolute top-1/2 left-0 transform  -translate-y-1/2">
+                                <div class="lg:w-4/6 md:w-full  absolute top-1/2 left-0 transform  -translate-y-1/2">
 
                                     <div data-swiper-parallax="200" class="wpo-hero-title-top">
                                         <span class="text-[19px] col:text-[16px] text-white font-base-font uppercase
-                                            mb-[30px] font-normal tracking-[2px]">Trusted
-                                            Business Partner.</span>
+                                            mb-[30px] font-normal tracking-[2px]">Welcome to Bezalel Frontiers.</span>
                                     </div>
                                     <div data-swiper-parallax="300" class="slide-title">
                                         <h2 class="text-[70px] font-black leading-[90px] mt-[10px] mb-[25px]
                                                 lg:text-[50px] md:text-[40px] md:leading-[55px] col:text-[30px]
                                                 col:leading-[35px] text-white col:mb-[20px]">
-                                            The Fastest way to achieve success</h2>
+                                            Research driven insights for making informed decisions.</h2>
 
                                     </div>
                                     <div data-swiper-parallax="400" class="slide-text">
-                                        <p class="text-[22px] text-[#e2e2e2] font-base-font
-                                            leading-[35px]
-                                                mb-[40px]
-                                            md:text-[18px] col:leading-[25px]
-                                            col:mb-[30px]">
-                                            It was popularised in the 1960s with the release sheets.
-                                            We bring the right people together.</p>
+                                        <p class="text-[22px] text-[#e2e2e2] font-base-font leading-[35px] mb-[40px] md:text-[18px] col:leading-[25px] col:mb-[30px]">
+                                            We are a data-driven and research-based management consulting firm that provides provide actionable insights that inform, inspire, and empower minds your organizations to make informed ideation, and business & management decisions.
+                                        </p>
                                     </div>
 
                                     <div data-swiper-parallax="500" class="slide-btn">
-                                        <a href="about.html" class="btn theme-btn rounded-none">
-                                            Explore more</a>
+                                        <a href="{{route('about')}}" class="btn theme-btn rounded-none">
+                                            Learn more</a>
                                     </div>
                                 </div>
                             </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -122,71 +122,39 @@
             <div class="grid grid-cols-12 items-center ">
                 <div class="col-span-6 md:col-span-12 ">
                     <div class="md:mb-[30px]">
-                        <h5 class="text-[21px] text-[#ff4a17] font-heading-font font-semibold sm:text-[18px]">Our
-                            Professional
-                            Skill’s</h5>
+                        <h5 class="text-[21px] text-[#ff4a17] font-heading-font font-semibold sm:text-[18px]">We are fully equipped to serve your needs</h5>
                         <h2 class="text-[60px] leading-[70px] mb-[30px] mt-[15px] text-[#0a272c] font-heading-font font-semibold
-                        xl:text-[50px] xl:leading-[60px] xl:mb-[20px] xl:mt-[10px] lg:text-[45px] lg:leading-[50px]
-                        sm:text-[35px] sm:leading-[50px]"> We provide Strategic Insights for Sustainable <span class="text-[#ff4a17]">Growth.</span></h2>
-                        <p class="text-[#687693] text-[16px] leading-[25px]">We data-driven and research-based with the goal of providing essential intelligence for organizations and their people for informed ideation, and business & management decisions. Our commitment to integrity, knowledge, and excellence sets us apart in the consulting industry.</p>
+                        xl:text-[40px] xl:leading-[60px] xl:mb-[20px] xl:mt-[10px] lg:text-[35px] lg:leading-[50px]
+                        sm:text-[35px] sm:leading-[50px]">We provide Strategic Insights and deliver <span class="text-[#ff4a17]">Growth.</span></h2>
+                        <div class="space-y-4">
+                            <p class="text-[#687693] text-[18px] leading-[25px]">Since our inception, {{$settings->company_name}} has been dedicated to delivering high-quality, research-driven insights that empower organizations to make informed decisions.</p>
+                            <p class="text-[#687693] text-[18px] leading-[25px]">We data-driven and research-based with the goal of providing essential intelligence for organizations and their people for informed ideation, and business & management decisions. Our commitment to integrity, knowledge, and excellence sets us apart in the consulting industry.</p>
+
+                            <div>
+                                <div class="mt-10">
+                                    <a class="theme-btn me-0" href="{{route('about')}}">Get in touch</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="col-span-6 md:col-span-12">
-                    <div
-                        class="ml-[70px] xl:ml-[50px] lg:ml-[30px] md:ml-0 p-[80px_70px] shadow-[0px_0px_35px_0px_rgba(20,33,43,0.1)] xl:p-[60px_50px] lg:p-[40px_30px]">
-                        <div class="mb-[30px] relative">
-                            <h5
-                                class="text-[22px] sm:text-[18px] mb-[10px] font-heading-font text-[#0a272c] font-semibold">
-                                Financial Investment</h5>
-                            <div class="h-[6px] w-full bg-[#f3f4f4] relative rounded-[10px] overflow-hidden">
-                                <div class="wow fadeInLeft h-[6px]  bg-[#ff4a17]" style="width: 75%"
-                                    data-wow-duration="0.5s" data-wow-delay=".3s" role="progressbar"
-                                    aria-valuenow="75" aria-valuemin="0" aria-valuemax="90">
+                    @if ($services->count())
+                        <div class="ml-[70px] xl:ml-[50px] lg:ml-[30px] md:ml-0 p-[80px_70px] shadow-[0px_0px_35px_0px_rgba(20,33,43,0.1)] xl:p-[60px_50px] lg:p-[40px_30px]">
+                            @forelse ($services as $service)
+                                <div class="mb-[30px] relative">
+                                    <div class="flex justify-between items-end mb-[10px]">
+                                        <h5 class="text-[22px] sm:text-[18px]  font-heading-font text-[#0a272c] max-w-[80%] font-semibold">{{$service->name}}</h5>
+                                        <span class="text-[22px] font-heading-font text-[#14212b] font-bold sm:text-[18px]">100%</span>
+                                    </div>
+                                    <div class="h-[6px] w-full bg-[#f3f4f4] relative rounded-[10px] overflow-hidden">
+                                        <div class="wow fadeInLeft h-[6px]  bg-[#ff4a17]" style="width: 100%" data-wow-duration="0.5s" data-wow-delay=".3s" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
                                 </div>
-                            </div>
-                            <span
-                                class="absolute right-0 top-[-10px] text-[22px] font-heading-font text-[#14212b] font-bold sm:text-[18px] sm:top-0">75%</span>
+                            @empty
+                            @endforelse
                         </div>
-                        <div class="mb-[30px] relative">
-                            <h5
-                                class="text-[22px] sm:text-[18px] mb-[10px] font-heading-font text-[#0a272c] font-semibold">
-                                Business Analysis</h5>
-                            <div class="h-[6px] w-full bg-[#f3f4f4] relative rounded-[10px] overflow-hidden">
-                                <div class="wow fadeInLeft h-[6px]  bg-[#ff4a17]" style="width: 80%"
-                                    data-wow-duration="0.7s" data-wow-delay=".3s" role="progressbar"
-                                    aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                                </div>
-                            </div>
-                            <span
-                                class="absolute right-0 top-[-10px] text-[22px] font-heading-font text-[#14212b] font-bold sm:text-[18px] sm:top-0">80%</span>
-                        </div>
-                        <div class="mb-[30px] relative">
-                            <h5
-                                class="text-[22px] sm:text-[18px] mb-[10px] font-heading-font text-[#0a272c] font-semibold">
-                                Tax Management</h5>
-                            <div class="h-[6px] w-full bg-[#f3f4f4] relative rounded-[10px] overflow-hidden">
-                                <div class="wow fadeInLeft h-[6px]  bg-[#ff4a17]" style="width: 85%"
-                                    data-wow-duration="0.9s" data-wow-delay=".3s" role="progressbar"
-                                    aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">
-                                </div>
-                            </div>
-                            <span
-                                class="absolute right-0 top-[-10px] text-[22px] font-heading-font text-[#14212b] font-bold sm:text-[18px] sm:top-0">85%</span>
-                        </div>
-                        <div class="relative">
-                            <h5
-                                class="text-[22px] sm:text-[18px] mb-[10px] font-heading-font text-[#0a272c] font-semibold">
-                                Financial Advices</h5>
-                            <div class="h-[6px] w-full bg-[#f3f4f4] relative rounded-[10px] overflow-hidden">
-                                <div class="wow fadeInLeft h-[6px]  bg-[#ff4a17]" style="width: 95%"
-                                    data-wow-duration="1.1s" data-wow-delay=".3s" role="progressbar"
-                                    aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">
-                                </div>
-                            </div>
-                            <span
-                                class="absolute right-0 top-[-10px] text-[22px] font-heading-font text-[#14212b] font-bold sm:text-[18px] sm:top-0">95%</span>
-                        </div>
-                    </div>
+                    @endif
                 </div>
             </div>
         </div>
