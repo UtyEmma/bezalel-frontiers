26b16edc4fd8014e9468af881e12527b537d2cb4
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2024-07-02 10:47:38 +0100

    Handled

--- resources/views/partials/home/banner.blade.php
+++ resources/views/partials/home/banner.blade.php
@@ -3,9 +3,10 @@
         <div class="swiper-wrapper">
             @forelse ($sliders as $slider)
                 <div class="swiper-slide">
-                    <div class="slide-inner slide-bg-image bg-[url({{$slider->image}})] before:absolute before:left-0
-                    before:top-0 before:w-full before:h-full before:bg-[#040128] before:opacity-[0.7] px-4"
-                        data-background="{{$slider->image}}">
+                    <div class="slide-inner px-3"
+                        data-background="{{$slider->image}}"
+                            style="background-image: linear-gradient(#04012890,#04012890), url({{asset('assets/imgs/bezalel-frontiers-business-meeting.jpg')}})"
+                        >
                         <!-- <div class="gradient-overlay"></div> -->
                         <div class="wraper">
                             <div class="relative h-[900px] lg:h-[600px]  md:h-[600px] sm:h-[500px]">
@@ -20,7 +21,7 @@
                                     </div>
 
                                     <div data-swiper-parallax="400" class="slide-text">
-                                        <p class="text-[22px] text-[#e2e2e2] font-base-font leading-[35px] mb-[40px] md:text-[18px] col:leading-[25px] col:mb-[30px]">{{$slider->subheading}}</p>
+                                        <p class="text-[22px] text-white font-base-font leading-[35px] mb-[40px] md:text-[18px] col:leading-[25px] col:mb-[30px]">{{$slider->subheading}}</p>
                                     </div>
 
                                     <div data-swiper-parallax="500" class="slide-btn">
@@ -33,15 +34,16 @@
                 </div>
             @empty
                 <div class="swiper-slide">
-                    <div class="slide-inner slide-bg-image before:absolute before:left-0 before:top-0 before:w-full before:h-full before:bg-[#040128] before:opacity-[0.7]" data-background="{{asset('assets/imgs/bezalel-frontiers-business-meeting.jpg')}} px-4" >
+                    <div class="slide-inner px-3" 
+                        style="background-image: linear-gradient(#04012890,#04012890), url({{asset('assets/imgs/bezalel-frontiers-business-meeting.jpg')}})"
+                    >
                         <!-- <div class="gradient-overlay"></div> -->
                         <div class="wraper">
                             <div class="relative h-[900px] lg:h-[680px]  md:h-[600px] sm:h-[500px]">
                                 <div class="lg:w-4/6 md:w-full  absolute top-1/2 left-0 transform  -translate-y-1/2">
 
                                     <div data-swiper-parallax="200" class="wpo-hero-title-top">
-                                        <span class="text-[19px] col:text-[16px] text-white font-base-font uppercase
-                                            mb-[30px] font-normal tracking-[2px]">Welcome to Bezalel Frontiers.</span>
+                                        <span class="text-[19px] col:text-[16px] text-white font-base-font uppercase mb-[30px] font-normal tracking-[2px]">Welcome to Bezalel Frontiers.</span>
                                     </div>
                                     <div data-swiper-parallax="300" class="slide-title">
                                         <h2 class="text-[70px] font-black leading-[90px] mt-[10px] mb-[25px]
@@ -51,7 +53,7 @@
 
                                     </div>
                                     <div data-swiper-parallax="400" class="slide-text">
-                                        <p class="text-[18px] text-[#e2e2e2] font-base-font leading-[35px] mb-[40px] col:leading-[25px] col:mb-[30px]">
+                                        <p class="text-[18px] text-white font-base-font leading-[35px] mb-[40px] col:leading-[25px] col:mb-[30px]">
                                             We are a data-driven and research-based management consulting firm that provides provide actionable insights that inform, inspire, and empower minds your organizations to make informed ideation, and business & management decisions.
                                         </p>
                                     </div>
